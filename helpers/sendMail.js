fb7dfc174ed1d691a5f2ed6d3c9d427fee9c758d
Andrews Opoku <89338797+aopoku255@users.noreply.github.com>
2024-12-05 06:11:31 +0000

    Update sendMail.js

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -5,19 +5,19 @@ require("dotenv").config();
 const sendMail = (email, mailBody) =>
   new Promise((resolve, reject) => {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: "mail.cariscabusinessforum.com",
       port: 465,
       secure: true,
       auth: {
-        user: "aopoku255@gmail.com",
-        pass: "emmicvgezezvqupw",
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     // write a function to send the mail
 
     const mailOptions = {
-      from: "CARISCA <info@carisca.knust.edu.gh>",
+      from: "CARISCA <info@cariscabusinessforum.com>",
       to: email,
       subject: "CARISCA BUSINESS FORUM",
       html: mailBody,
